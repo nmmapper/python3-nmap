b49b608f0eefd7a68eedce08490686e039a76542
Wangolo Joel <wangolo@outlook.com>
2019-12-05 13:11:23 +0300

    Added more version detection

---
+++ docs/index.rst
@@ -0,0 +1,106 @@
+.. python3-nmap documentation master file, created by
+   sphinx-quickstart on Thu Dec  5 12:49:59 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to python3-nmap's documentation!
+========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+Home page
+=========
+* `https://www.nmmapper.com <https://www.nmmapper.com>`_
+
+Repository and source code
+==========================
+* `https://github.com/wangoloj/python3-nmap <https://github.com/wangoloj/python3-nmap>`_
+
+Rationale
+=========
+There is python-nmap projects out there hosted on bitbucket, which is the basic of our `online port scanner <https://www.nmmapper.com/st/networkmapper/nmap/online-port-scanning/>`_ at `Nmmapper <https://www.nmmapper.com>`_ But we wanted to extend our `online port scanner <https://www.nmmapper.com/st/networkmapper/nmap/online-port-scanning/>`_  with nmap features like running nmap scripts online. The existing projects does it very well, in fact we used the existing python-nmap project to run nmap's dns-brute script on our `subdomain finder tool <https://www.nmmapper.com/sys/tools/subdomainfinder/>`_
+. 
+
+But we wanted something that defines each nmap command and  each nmap script as a python3 function that we can call like calling python3 function. for example
+
+.. code-block:: python
+   :linenos:
+
+    import nmap3
+    nmap = Nmap()
+    
+    result = nmap.nmap_version_detection("nmmapper.com")
+
+    # This is equivalent to nmap's
+    # nmap  nmmapper.com  -sV
+    # Except we add 'oX' to be /usr/bin/nmap  -oX  -  nmmapper.com  -sV
+    # 
+    # result Output 
+    [
+    {
+        "cpe": [
+            {
+                "cpe": "cpe:/o:linux:linux_kernel"
+            }
+        ],
+        "port": "80",
+        "protocol": "tcp",
+        "service": {
+            "conf": "10",
+            "extrainfo": "Ubuntu",
+            "method": "probed",
+            "name": "http",
+            "ostype": "Linux",
+            "product": "nginx",
+            "version": "1.14.0"
+        }
+    },
+    {
+        "cpe": [
+            {
+                "cpe": "cpe:/o:linux:linux_kernel"
+            }
+        ],
+        "port": "443",
+        "protocol": "tcp",
+        "service": {
+            "conf": "10",
+            "extrainfo": "Ubuntu",
+            "method": "probed",
+            "name": "http",
+            "ostype": "Linux",
+            "product": "nginx",
+            "tunnel": "ssl",
+            "version": "1.14.0"
+        }
+    },
+    {
+        "cpe": [
+            {
+                "cpe": "cpe:/o:linux:linux_kernel"
+            }
+        ],
+        "port": "2000",
+        "protocol": "tcp",
+        "service": {
+            "conf": "10",
+            "extrainfo": "Ubuntu Linux; protocol 2.0",
+            "method": "probed",
+            "name": "ssh",
+            "ostype": "Linux",
+            "product": "OpenSSH",
+            "version": "7.6p1 Ubuntu 4ubuntu0.3"
+        }
+    }
+    ]
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- nmap3/nmap3.py
+++ nmap3/nmap3.py
@@ -1,23 +1,23 @@
 #  nmap3.py
-#  
+#
 #  Copyright 2019 Wangolo Joel <wangolo@ldap.testlumiotic.com>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+#
+#
 import csv
 import io
 import os
@@ -43,81 +43,102 @@ class Nmap(object):
     def __init__(self, path=None):
         """
         Module initialization
-        
+
         :param path: Path where nmap is installed on a user system. On linux system it's typically on /usr/bin/nmap.
         """
-        
+
         self.nmaptool = path if path else get_nmap_path()
         self.default_args = "{nmap}  {outarg}  -  "
         self.maxport = 65389
         self.host = ""
         self.top_ports = dict()
-        
+
     def default_command(self):
         """
         Returns the default nmap command
         that will be chained with all others
-        eg nmap -oX - 
+        eg nmap -oX -
         """
         return self.default_args.format(nmap=self.nmaptool, outarg="-oX")
-        
+
     def scan_top_ports(self, host, default=10):
         """
         Perform nmap's top ports scan
-        
+
         :param: host can be IP or domain
         :param: default is the default top port
-        
+
         This top port requires root previledges
         """
-        
+
         if(default > self.maxport):
             raise ValueError("Port can not be greater than default 65389")
-        self.host = host 
-        
+        self.host = host
+
         top_port_args = " {host} --top-ports {default}".format(host=host, default=default)
         top_port_command = self.default_command() + top_port_args
         top_port_shlex = shlex.split(top_port_command) # prepare it for popen
-        
+
         # Run the command and get the output
         output = self.run_command(top_port_shlex)
         if not output:
             # Probaby and error was raise
             raise ValueError("Unable to perform requested command")
-        
+
         # Begin parsing the xml response
         xml_root = self.get_xml_et(output)
         self.top_ports = self.filter_top_ports(xml_root)
-        return self.top_ports 
-        
+        return self.top_ports
+
     def nmap_dns_brute_script(self, host, dns_brute="--script dns-brute.nse"):
         """
         Perform nmap scan usign the dns-brute script
-        
+
         :param: host can be IP or domain
         :param: default is the default top port
-        
+
         nmap -oX - nmmapper.com --script dns-brute.nse
         """
-        self.host = host 
-        
+        self.host = host
+
         dns_brute_args = "{host}  {default}".format(host=host, default=dns_brute)
-        
+
         dns_brute_command = self.default_command() + dns_brute_args
         dns_brute_shlex = shlex.split(dns_brute_command) # prepare it for popen
-        
+
         # Run the command and get the output
         output = self.run_command(dns_brute_shlex)
-        
+
         # Begin parsing the xml response
         xml_root = self.get_xml_et(output)
         subdomains = self.filter_subdomains(xml_root)
         return subdomains
-        
+
+    def nmap_version_detection(self, host, arg="-sV"):
+        """
+        Perform nmap scan usign the dns-brute script
+
+        :param: host can be IP or domain
+
+        nmap -oX - nmmapper.com --script dns-brute.nse
+        """
+        self.host = host
+
+        command_args = "{host}  {default}".format(host=host, default=arg)
+        command = self.default_command() + command_args
+        dns_brute_shlex = shlex.split(command) # prepare it for popen
+
+        # Run the command and get the output
+        output = self.run_command(dns_brute_shlex)
+        xml_root = self.get_xml_et(output)
+
+        services = self.version_parser(xml_root)
+        return services
+
     def run_command(self, cmd):
         """
         Runs the nmap command using popen
-        
+
         @param: cmd--> the command we want run eg /usr/bin/nmap -oX -  nmmapper.com --top-ports 10
         """
         sub_proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
@@ -129,21 +150,21 @@ class Nmap(object):
         else:
             # Response is bytes so decode the output and return
             return output.decode('utf8').strip()
-    
+
     def get_xml_et(self, command_output):
         """
         @ return xml ET
         """
         return ET.fromstring(command_output)
-        
+
     def filter_top_ports(self, xmlroot):
         """
-        Given the xmlroot return the all the ports that are open from 
+        Given the xmlroot return the all the ports that are open from
         that tree
         """
         try:
             port_results = []
-            
+
             scanned_host = xmlroot.find("host")
             if(scanned_host):
                 ports = scanned_host.find("ports").findall("port")
@@ -151,73 +172,72 @@ class Nmap(object):
                 # slowly parse what is required
                 for port in ports:
                     open_ports = {}
-                    
+
                     open_ports["protocol"]=port.attrib.get("protocol")
                     open_ports["port"]=port.attrib.get("portid")
-                    
+
                     if port.find('state') != None:
                         open_ports["state"]=port.find("state").attrib.get("state")
                         open_ports["reason"]=port.find('state').attrib.get("reason")
                         open_ports["reason_ttl"]=port.find("state").attrib.get("reason_ttl")
-              
+
                     if  port.find("service") != None:
                         open_ports["service"]=port.find("service").attrib
-                        
+
                     port_results.append(open_ports)
-                
+
         except Exception as e:
             raise(e)
         else:
             return port_results
-            
-    def filter_subdomains(self, xmlroot):
+
+    def version_parser(self, xmlroot):
         """
-        Given the xmlroot return the all the ports that are open from 
-        that tree
+        Parse version detected
         """
         try:
-            subdomains_list = []
-            
-            scanned_host = xmlroot.find("host")
-            if(scanned_host):
-                hostscript = scanned_host.find("hostscript")
-                
-                script = None
-                first_table = None
-                final_result_table = None
-                
-                if(hostscript):
-                    script = hostscript.find("script")
-                    
-                if(hostscript):
-                    first_table = script.find("table")
-                    
-                if(first_table):
-                    final_result_table = first_table.findall("table")
-                
-                for table in final_result_table:
-                    script_results = dict()
-                    elem = table.findall("elem")
-                    
-                    if(len(elem) >= 2):
-                        address = elem[0]
-                        hostname = elem[1]
+            service_version = []
+
+            host = xmlroot.find("host")
+            if(host):
+                ports  = host.find("ports")
+                port_service = None
+
+                if(ports):
+                    port_service = ports.findall("port")
+
+                if(port_service):
+                    for port in port_service:
+                        service = {}
+
+                        service["protocol"]=port.attrib.get("protocol")
+                        service["port"]=port.attrib.get("portid")
+
+                        if(port.find("state")):
+                            for s in port.find("state").attrib:
+                                service[s]=port.find("state").attrib.get(s)
+
+                        if(port.find("service")):
+                            service["service"]=port.find("service").attrib
+                            
+                            for cp in port.find("service").findall("cpe"):
+                                cpe_list = []
+                                cpe_list.append({"cpe":cp.text})
+                                service["cpe"]=cpe_list
+                                
+                        service_version.append(service)
                         
-                        script_results["address"]=address.text 
-                        script_results["hostname"]=hostname.text 
-                        subdomains_list.append(script_results)
-
-
         except Exception as e:
             raise(e)
         else:
-            return subdomains_list
-            
+            return service_version
+
 if __name__=="__main__":
     parser = argparse.ArgumentParser(prog="Python3 nmap")
     parser.add_argument('-d', '--d', help='Help', required=True)
     args = parser.parse_args()
 
     nmap = Nmap()
-    result = nmap.nmap_dns_brute_script(args.d)
-    print(json.dumps(result, indent=4, sort_keys=True))
+    result = nmap.nmap_version_detection(args.d)
+    print(result)
+    #print(json.dumps(result, indent=4, sort_keys=True))
