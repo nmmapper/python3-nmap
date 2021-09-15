9783672c1de6dbfa9f4b70780143286fd0ab6f53
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2021-09-15 13:34:39 +0300

    Update

--- nmap3/exceptions.py
+++ nmap3/exceptions.py
@@ -27,20 +27,23 @@ __author__ = 'Wangolo Joel (inquiry@nmapper.com)'
 __version__ = '1.4.9'
 __last_modification__ = '2029/12/11'
 
+
 class NmapNotInstalledError(Exception):
     """Exception raised when nmap is not installed"""
-    
-    def __init__(self, message="Nmap is either not installed or we couldn't locate nmap path Please ensure nmap is installed"):
-        self.message = message 
+
+    def __init__(self,
+                 message="Nmap is either not installed or we couldn't locate nmap path Please ensure nmap is installed"):
+        self.message = message
         super().__init__(message)
-        
+
+
 class NmapXMLParserError(Exception):
     """Exception raised when we can't parse the output"""
-    
+
     def __init__(self, message="Unable to parse xml output"):
-        self.message = message 
+        self.message = message
         super().__init__(message)
+
 
 class NmapExecutionError(Exception):
     """Exception raised when en error occurred during nmap call"""
-
