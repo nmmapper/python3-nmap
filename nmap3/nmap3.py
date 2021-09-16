1a011e0fd9dee62d82eb7d031101b07d065c0064
Christian Clauss <cclauss@me.com>
2021-09-16 18:12:19 +0200

    Update nmap3.py

--- nmap3/nmap3.py
+++ nmap3/nmap3.py
@@ -160,7 +160,7 @@ class Nmap(object):
         # Run the command and get the output
         output = self.run_command(scan_shlex, timeout=timeout)
         if not output:
-            # Probably and error was raise
+            # Probably an error was raised
             raise ValueError("Unable to perform requested command")
 
         # Begin parsing the xml response
